28b782592afc63187824f5ec77c6d2944f888451
bcmfranco <bruno@codev.com.ar>
2024-02-05 18:16:40 -0300

    Ad all

---
+++ public/components/brander.vue
@@ -0,0 +1,69 @@
+<template>
+
+  <div id="gumcom_brander">
+    <div id="logotype">
+      <div>DE</div>
+    </div>
+    <div id="isotype">
+      <h1>desky</h1>
+      <h2>virtual desk organizer</h2>
+      <Navbarr />
+    </div>
+  </div>
+
+
+</template>
+
+
+<style scoped>
+
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+  #gumcom_brander{
+    font-family: Roboto;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-items: center;
+  }
+
+  #gumcom_brander #logotype{
+    font-size: 25px;
+    color: #723f78;
+    width: 50px; /* Ajusta el ancho según tus necesidades */
+    height: 50px; /* Ajusta la altura según tus necesidades */
+    border: 2px solid #723f78; /* Color del borde */
+    border-radius: 8px; /* Ajusta el radio del borde según tus necesidades */
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1); /* Añade una sombra */
+    margin: 10px;
+  }
+
+  #gumcom_brander #isotype h1 {
+    color: #723f78;
+    margin: 0px;
+    padding: 0px;
+  }
+  
+  #gumcom_brander #isotype h2 {
+    font-size: 24px;
+    color: #723f78;
+    margin-bottom: 20px;
+    margin: 0px;
+    padding: 0px;
+  }
+
+
+  @media (max-width: 480px) {
+
+    #gumcom_brander{
+      flex-direction: column;
+      align-items: start;
+    }
+
+
+  }
+
+
+
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <div id="container">
+
+    <div>
+      <Brander />
+    </div>
+
+    <div>core</div>
+
+  </div>
+</template>
+
+<script>
+
+// Poner todo el header dentro de un componente, y la navbarr adentro
+
+import Brander from '../../public/components/brander.vue';
+
+export default {
+  components: {
+    Brander
+  },
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+  }
+};
+</script>
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap');
+
+#container{
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 1fr;
+  align-items: center;
+  justify-items: center;
+  height: 100vh;
+  font-family: Nunito;
+}
+
+#calculator_form {
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+#calculator_form h2{
+  color: #26a69a;
+}
+
+#isotype h1 {
+  color: #26a69a;
+  margin: 0px;
+  padding: 0px;
+}
+
+#isotype h2 {
+  font-size: 24px;
+  color: #26a69a;
+  margin-bottom: 20px;
+  margin: 0px;
+  padding: 0px;
+}
+
+label {
+  font-size: 16px;
+  color: #26a69a;
+  margin-bottom: 8px;
+}
+
+input {
+  font-size: 16px;
+  padding: 10px;
+  border: 1px solid #26a69a;
+  border-radius: 4px;
+  margin-bottom: 20px;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+input[disabled] {
+  background-color: #f5f5f5;
+}
+
+button {
+  font-size: 16px;
+  padding: 10px 20px;
+  background-color: #26a69a;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: #00796b;
+}
+
+@media (max-width: 480px) {
+  #container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  #container #brand{
+    /* grid-template-columns: 1fr; */
+
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+
+</style>
